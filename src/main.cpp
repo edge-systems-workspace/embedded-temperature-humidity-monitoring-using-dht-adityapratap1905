b691088cb3513bdbb9478c35ec66c396bf44ce58
adityapratap1905 <adityapratap1905@gmail.com>
2026-02-27 16:12:36 +0530

    remove TODO comments for DHT sensor setup

--- src/main.cpp
+++ src/main.cpp
@@ -13,35 +13,12 @@
 
 #include <DHT.h>
 
-
+void  loop()
+{
+    
+}
 
 void setup() {
 
-    // TODO 4:
-    // Initialize Serial communication (9600 baud rate)
 
-    // TODO 5:
-    // Initialize the DHT sensor
-
-    // TODO 6:
-    // Print a system initialization message
-}
-
-void loop() {
-
-    // TODO 7:
-    // Read humidity value from sensor
-
-    // TODO 8:
-    // Read temperature value from sensor
-
-    // TODO 9:
-    // Check if either reading failed using isnan()
-    // If failed, print error message and return
-
-    // TODO 10:
-    // Print formatted temperature and humidity values
-
-    // TODO 11:
-    // Add a 2-second delay before next reading
 }
