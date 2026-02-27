d2faf8adfa7001d7243b40cbf2e72f09bc3eab24
adityapratap1905 <adityapratap1905@gmail.com>
2026-02-27 16:12:58 +0530

    remove unnecessary comments and clean up code structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,8 @@
-#include <Arduino.h>
-/**
- * @file main.ino
- * @brief Embedded Temperature and Humidity Monitoring using DHT11
- * @author YOUR_NAME
- * @date YYYY-MM-DD
- *
- * @details
- * This program reads environmental data from the DHT11 sensor
- * and displays temperature and humidity values on Serial Monitor.
- * Students must complete the TODO sections.
- */
 
-#include <DHT.h>
 
 void  loop()
 {
-    
+
 }
 
 void setup() {
